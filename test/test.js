20c6e6ea44676ed0fb91c971354722577a8bad2d
Sadman Rahman <sikdarsadmanrahman@gmail.com>
2025-10-01 09:07:48 +0600

    developed APIs for learnig

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const request = require('supertest');
 
-const app = require('../app');
+const app = require('../app.js');
 
 describe('/api/minions routes', function() {
   let fakeDb = require('../server/db.js');
